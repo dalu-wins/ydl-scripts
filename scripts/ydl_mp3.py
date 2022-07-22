d96a644672a9166ddfb0c4055b00b3520e02e414
daniilwins <daniil.wins@gmail.com>
2022-07-22 13:15:18 +0200

    Added download archive

--- scripts/ydl_mp3.py
+++ scripts/ydl_mp3.py
@@ -3,7 +3,8 @@ from datetime import date
 
 url = 'https://www.youtube.com/watch?v=dQw4w9WgXcQ&ab_channel=RickAstley'
 options = {
-    'outtmpl': './downloads/audio/{directory}/%(title)s-%(artist)s.%(ext)s'.format(directory=date.today()),
+    'download_archive': './downloads/audio/{directory}/'.format(directory=date.today()),
+    'outtmpl': '%(title)s-%(artist)s.%(ext)s',
     'format': 'bestaudio/best',
     'postprocessors': [{
         'key': 'FFmpegExtractAudio',
