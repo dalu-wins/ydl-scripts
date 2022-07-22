586db62e7c64754cf57cb90fc2d338f9268fbb83
daniilwins <daniil.wins@gmail.com>
2022-07-22 17:11:30 +0200

    Final commit

--- scripts/ydl_mp3.py
+++ scripts/ydl_mp3.py
@@ -1,20 +1,39 @@
 import youtube_dl
 from datetime import date
 
-urls = [input("Enter playlist or video url:\t")]
-options = {
-    'outtmpl': './downloads/audio/{directory}/%(title)s.%(ext)s'.format(directory=date.today()),
-    'format': 'bestaudio/best',
-    'addmetadata': True,
-    'ignoreerrors': True,
-    'continuedl': True,
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
-    }, {
-        'key': 'FFmpegMetadata',
-    }],
-}
-with youtube_dl.YoutubeDL(options) as ydl:
-    ydl.download(urls)
+global url
+global options
+
+
+def add_url():
+    global url
+    url.append(input("Enter url:\t"))
+
+
+def set_options():
+    global options
+    options = {
+        'outtmpl': './downloads/audio/{directory}/%(title)s.%(ext)s'.format(directory=date.today()),  # custom download path
+        'format': 'bestaudio/best',
+        'addmetadata': True,
+        'ignoreerrors': True,  # skip to next song if error occurs
+        'continuedl': True,  # continue download if cancelled before
+        'postprocessors': [{
+            'key': 'FFmpegExtractAudio',  # only audio
+            'preferredcodec': 'mp3',  # as .mp3
+            'preferredquality': '192',  # best quality preferred
+        }, {
+            'key': 'FFmpegMetadata',  # needed for metadata saving
+        }],
+    }
+
+
+def start_download():
+    with youtube_dl.YoutubeDL(options) as ydl:
+        ydl.download(url)
+
+
+if __name__ == '__main__':
+    add_url()
+    set_options()
+    start_download()
--- scripts/ydl_mp4.py
+++ scripts/ydl_mp4.py
@@ -1,19 +1,38 @@
 import youtube_dl
 from datetime import date
 
-urls = [input("Enter playlist or video url:\t")]
-options = {
-    'outtmpl': '../downloads/video/{directory}/%(title)s.%(ext)s'.format(directory=date.today()),
-    'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]',
-    'addmetadata': True,
-    'ignoreerrors': True,
-    'continuedl': True,
-    'postprocessors': [{
-        'key': 'FFmpegVideoConvertor',
-        'preferedformat': 'mp4',
-    }, {
-        'key': 'FFmpegMetadata',
-    }],
-}
-with youtube_dl.YoutubeDL(options) as ydl:
-    ydl.download(urls)
+global url
+global options
+
+
+def add_url():
+    global url
+    url.append(input("Enter url:\t"))
+
+
+def set_options():
+    global options
+    options = {
+        'outtmpl': '../downloads/video/{directory}/%(title)s.%(ext)s'.format(directory=date.today()),
+        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]',
+        'addmetadata': True,
+        'ignoreerrors': True,
+        'continuedl': True,
+        'postprocessors': [{
+            'key': 'FFmpegVideoConvertor',
+            'preferedformat': 'mp4',
+        }, {
+            'key': 'FFmpegMetadata',
+        }],
+    }
+
+
+def start_download():
+    with youtube_dl.YoutubeDL(options) as ydl:
+        ydl.download(url)
+
+
+if __name__ == '__main__':
+    add_url()
+    set_options()
+    start_download()
