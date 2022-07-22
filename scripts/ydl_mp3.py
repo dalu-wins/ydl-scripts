d2887988acf47d70fe3538f16e1286d03ff25e6f
daniilwins <daniil.wins@gmail.com>
2022-07-22 20:51:31 +0200

    Only comments missing

--- scripts/ydl_mp3.py
+++ scripts/ydl_mp3.py
@@ -1,39 +1,22 @@
 import youtube_dl
 from datetime import date
 
-global url
-global options
+url = [input("Enter url:\t")]
+options = {
+    'outtmpl': './downloads/audio/{directory}/%(title)s.%(ext)s'.format(directory=date.today()),  # custom download path
+    'format': 'bestaudio/best',
+    'addmetadata': True,
+    'ignoreerrors': True,       # skip to next song if error occurs
+    'continuedl': True,         # continue download if cancelled before
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',    # only audio
+        'preferredcodec': 'mp3',        # as .mp3
+        'preferredquality': '192',      # best quality preferred
+    }, {
+        'key': 'FFmpegMetadata',  # needed for metadata saving
+    }],
+}
 
+with youtube_dl.YoutubeDL(options) as ydl:
+    ydl.download(url)
 
-def add_url():
-    global url
-    url.append(input("Enter url:\t"))
-
-
-def set_options():
-    global options
-    options = {
-        'outtmpl': './downloads/audio/{directory}/%(title)s.%(ext)s'.format(directory=date.today()),  # custom download path
-        'format': 'bestaudio/best',
-        'addmetadata': True,
-        'ignoreerrors': True,  # skip to next song if error occurs
-        'continuedl': True,  # continue download if cancelled before
-        'postprocessors': [{
-            'key': 'FFmpegExtractAudio',  # only audio
-            'preferredcodec': 'mp3',  # as .mp3
-            'preferredquality': '192',  # best quality preferred
-        }, {
-            'key': 'FFmpegMetadata',  # needed for metadata saving
-        }],
-    }
-
-
-def start_download():
-    with youtube_dl.YoutubeDL(options) as ydl:
-        ydl.download(url)
-
-
-if __name__ == '__main__':
-    add_url()
-    set_options()
-    start_download()
--- scripts/ydl_mp4.py
+++ scripts/ydl_mp4.py
@@ -1,18 +1,8 @@
 import youtube_dl
 from datetime import date
 
-global url
-global options
-
-
-def add_url():
-    global url
-    url.append(input("Enter url:\t"))
-
-
-def set_options():
-    global options
-    options = {
+url = [input("Enter url:\t")]
+options = {
         'outtmpl': '../downloads/video/{directory}/%(title)s.%(ext)s'.format(directory=date.today()),
         'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]',
         'addmetadata': True,
@@ -26,13 +16,5 @@ def set_options():
         }],
     }
 
-
-def start_download():
-    with youtube_dl.YoutubeDL(options) as ydl:
-        ydl.download(url)
-
-
-if __name__ == '__main__':
-    add_url()
-    set_options()
-    start_download()
+with youtube_dl.YoutubeDL(options) as ydl:
+    ydl.download(url)
