aef89bf1088e961033da4a29a489b00862270a2b
daniilwins <daniil.wins@gmail.com>
2022-07-22 16:39:03 +0200

    Added mp4 skript Minor changes in mp3 script

--- scripts/ydl_mp4.py
+++ scripts/ydl_mp4.py
@@ -3,15 +3,14 @@ from datetime import date
 
 urls = [input("Enter playlist or video url:\t")]
 options = {
-    'outtmpl': './downloads/audio/{directory}/%(title)s.%(ext)s'.format(directory=date.today()),
-    'format': 'bestaudio/best',
+    'outtmpl': '../downloads/video/{directory}/%(title)s.%(ext)s'.format(directory=date.today()),
+    'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]',
     'addmetadata': True,
     'ignoreerrors': True,
     'continuedl': True,
     'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': '192',
+        'key': 'FFmpegVideoConvertor',
+        'preferedformat': 'mp4',
     }, {
         'key': 'FFmpegMetadata',
     }],
